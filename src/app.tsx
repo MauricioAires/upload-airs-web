d48a19f81e1fab7cbfe8f08be0402547c7e23112
MauricioAires <mauricioaires015@gmail.com>
2023-09-16 20:59:52 -0300

    feat(app) save temperature, videoId and template

--- src/app.tsx
+++ src/app.tsx
@@ -14,8 +14,23 @@ import {
 import { Slider } from "@/components/ui/slider";
 import { VideoInputForm } from "./components/video-input-form";
 import { PromptSelect } from "./components/prompt-select";
+import { useState } from "react";
 
 export function App() {
+  const [temperature, setTemperature] = useState(0.5);
+  const [videoId, setVideoId] = useState<string | null>(null);
+
+  function handlePromptSelected(template: string) {
+    console.log(template);
+  }
+
+  function handleSubmit(e: HTMLFormElement) {
+    e.preventDefault();
+
+    console.log(videoId);
+
+    console.log(temperature);
+  }
   return (
     <div className="min-h-screen flex flex-col">
       <div className="px-6 py-3 flex items-center justify-between border-b">
@@ -53,13 +68,13 @@ export function App() {
           </p>
         </div>
         <aside className="w-80 space-y-6">
-          <VideoInputForm />
+          <VideoInputForm onVideoUploaded={setVideoId} />
           <Separator />
 
           <form className="space-y-6">
             <div className="space-y-2">
               <Label>Prompt</Label>
-              <PromptSelect />
+              <PromptSelect onPromptSelected={handlePromptSelected} />
             </div>
             <div className="space-y-2">
               <Label>Modelo</Label>
@@ -81,7 +96,13 @@ export function App() {
             <div className="space-y-4">
               <Label>Temperatura</Label>
 
-              <Slider min={0} max={1} step={0.1} defaultValue={[0.5]} />
+              <Slider
+                min={0}
+                max={1}
+                step={0.1}
+                defaultValue={[temperature]}
+                onValueChange={(value) => setTemperature(value[0])}
+              />
               <span className="block text-xs text-muted-foreground italic leading-relaxed">
                 Valores mais altos tendem a deixar o resultado mais criativos e
                 com possíveis erros.
