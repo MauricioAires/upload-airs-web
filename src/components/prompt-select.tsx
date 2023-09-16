c248e6f1ed2ac985b41900013260d5eaea6f1182
MauricioAires <mauricioaires015@gmail.com>
2023-09-16 18:58:36 -0300

    feat(prompt select) list option on server

---
+++ src/components/prompt-select.tsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "./ui/select";
+import { api } from "@/lib/axios";
+
+interface Prompt {
+  id: string;
+  title: string;
+  template: string;
+}
+
+export function PromptSelect() {
+  const [prompts, setPrompts] = useState<Prompt[] | null>(null);
+
+  useEffect(() => {
+    api.get("/prompts").then((response) => {
+      setPrompts(response.data);
+    });
+  }, []);
+
+  return (
+    <Select>
+      <SelectTrigger>
+        <SelectValue placeholder="Selecione um prompt..." />
+      </SelectTrigger>
+
+      <SelectContent>
+        {prompts?.map((prompt) => (
+          <SelectItem key={prompt.id} value={prompt.template}>
+            {prompt.title}
+          </SelectItem>
+        ))}
+      </SelectContent>
+    </Select>
+  );
+}
