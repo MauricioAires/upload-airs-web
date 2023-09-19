179f589ad9a59a87c63ac88b4800dd5bf3d3c167
MauricioAires <mauricioaires015@gmail.com>
2023-09-18 23:04:22 -0300

    refactor(main) remover provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
 import { App } from "./app.tsx";
 
 import "./index.css";
-import { GenerateCompletionProvider } from "./context/generate-completion.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <GenerateCompletionProvider>
-      <App />
-    </GenerateCompletionProvider>
+    <App />
   </React.StrictMode>,
 );
