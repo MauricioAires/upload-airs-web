57d95857b8eb313c6383064832e2a61854e70703
MauricioAires <mauricioaires015@gmail.com>
2023-09-18 23:29:00 -0300

    feat(mode toggle) change theme

---
+++ src/components/mode-toggle.tsx
@@ -0,0 +1,20 @@
+import { Moon, Sun } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+
+import { useTheme } from "@/components/theme-provider";
+
+export function ModeToggle() {
+  const { setTheme, theme } = useTheme();
+
+  return (
+    <Button
+      onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+      variant="outline"
+      size="icon"
+    >
+      <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+      <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+    </Button>
+  );
+}
