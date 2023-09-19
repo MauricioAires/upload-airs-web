2a3c164a9f4ef844f1ff5330c1e1a0829f63fd74
MauricioAires <mauricioaires015@gmail.com>
2023-09-18 22:57:33 -0300

    refactor(prompt select) abstract functions

--- src/components/prompt-select.tsx
+++ src/components/prompt-select.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react";
 import {
   Select,
   SelectContent,
@@ -6,24 +5,12 @@ import {
   SelectTrigger,
   SelectValue,
 } from "./ui/select";
-import { api } from "@/lib/axios";
+
 import { useGenerateCompletion } from "@/context/generate-completion";
 
-interface Prompt {
-  id: string;
-  title: string;
-  template: string;
-}
-
 export function PromptSelect() {
-  const { setInput } = useGenerateCompletion();
-  const [prompts, setPrompts] = useState<Prompt[] | null>(null);
-
-  useEffect(() => {
-    api.get("/prompts").then((response) => {
-      setPrompts(response.data);
-    });
-  }, []);
+  const { setInput, prompts, promptSelectedId, setPromptSelectedId } =
+    useGenerateCompletion();
 
   function handlePromptSelected(promptId: string) {
     const selectedPrompt = prompts?.find((prompt) => prompt.id === promptId);
@@ -32,11 +19,16 @@ export function PromptSelect() {
       return;
     }
 
+    setPromptSelectedId(promptId);
+
     setInput(selectedPrompt.template);
   }
 
   return (
-    <Select onValueChange={handlePromptSelected}>
+    <Select
+      defaultValue={promptSelectedId}
+      onValueChange={handlePromptSelected}
+    >
       <SelectTrigger>
         <SelectValue placeholder="Selecione um prompt..." />
       </SelectTrigger>
