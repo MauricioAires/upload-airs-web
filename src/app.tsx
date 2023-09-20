2497f2fb24809c9272926fc4a80625b86a2f0272
MauricioAires <mauricioaires015@gmail.com>
2023-09-20 13:21:33 -0300

    feat(app) add toast provider

--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,18 @@
 import { Home } from "@/pages/home";
 import { GenerateCompletionProvider } from "@/context/generate-completion";
 import { ThemeProvider } from "@/components/theme-provider";
+import { Toaster as ToasterProvider } from "@/components/ui/toaster";
 
 export function App() {
   return (
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <GenerateCompletionProvider>
-        <Home />
-      </GenerateCompletionProvider>
-    </ThemeProvider>
+    <>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <GenerateCompletionProvider>
+          <Home />
+        </GenerateCompletionProvider>
+      </ThemeProvider>
+
+      <ToasterProvider />
+    </>
   );
 }
